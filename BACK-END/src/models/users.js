771b9a93c3810ecb218b3c309dde64b8c894a804
Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
2022-12-22 21:08:18 -0300

    Update users.js

--- BACK-END/src/models/users.js
+++ BACK-END/src/models/users.js
@@ -3,9 +3,10 @@ import mongoose, { Schema } from "mongoose";
 const UserSchema = new mongoose.Schema(
      {
        id: {type: String},
-       nome: {type: String, require: true}, 
-       senha: {type: String, require: true}
-        }
+       name: {type: String, require: true}, 
+       password: {type: String, require: true},
+       category: {type: String, require: true}
+      }
 );
 
 const user= mongoose.model('user', UserSchema);
