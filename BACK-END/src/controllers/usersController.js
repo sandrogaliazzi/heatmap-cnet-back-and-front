a220e26461fe0967e32c104b75795be2466b33d4
Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
2023-01-10 08:28:56 -0300

    Squashed commit of the following:
    commit 0768d6e4c3bdff3357bc08c6ded135b9960669cd
    Author: Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
    Date:   Mon Jan 9 15:22:54 2023 -0300
    
        end point nova user por id
    
        em prod.
    
    commit 9fc5fd6d4c6c914091c5d666f2719b1c781af5cb
    Author: Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
    Date:   Sat Jan 7 09:58:35 2023 -0300
    
        .delete users
    
    commit 28c133031fc2e4150bb4fef3b10f306e2f32cf5d
    Author: Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
    Date:   Sat Jan 7 09:14:44 2023 -0300
    
        .put users

--- BACK-END/src/controllers/usersController.js
+++ BACK-END/src/controllers/usersController.js
@@ -14,6 +14,13 @@ static ListarUsers = (req, res) => {
 })
 }
 
+static ListarUsersPorId = (req, res) => {
+  const id = req.params.id;
+  user.findById(id, (err, user)=>{
+  res.status(200).json(user)
+})
+}
+
 static CadastrarUser = (req, res) => {
     let users = new user(req.body);
     users.save((err) =>{
@@ -24,87 +31,6 @@ static CadastrarUser = (req, res) => {
         }
     })
 }
-
-  static RegisterUser = async (req, res) => {
-    try {
-      // Get user input
-      let { name, password, category } = req.body;
-  
-      // Validate user input
-      if (!(name && password )) {
-        res.status(400).send("All input is required");
-      }
-  
-      // check if user already exist
-      // Validate if user exist in our database
-      let oldUser = await user.find({name});
-  
-      if (!oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-      } else {
-  
-      //Encrypt user password
-     let encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create user in our database
-      let usuario = await user.create({
-        name,
-        category,
-        password: encryptedPassword,
-      });
-      
-      // Create token
-      let token = jwt.sign(
-        { user_id: usuario._id, name },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "180h",
-        }
-      );
-      // save user token
-      
-        usuario.save( usuario.token = token);
-  
-      // return new user
-      res.status(201).send({message: `${usuario.name} - cadastrado.`});
-    }} catch (err) {
-      console.log(err);
-    }
-  };
-
-  static userLogin = async (req, res) => {
-    try {
-      // Get user input
-      let { name, password } = req.body;
-  
-      // Validate user input
-      if (!(name && password)) {
-        res.status(400).send("All input is required");
-      } 
-      // Validate if user exist in our database
-      let usuario = await user.findOne({ name });
-  
-      if (usuario && (await bcrypt.compare(password, usuario.password))) {
-        // Create token
-        let token = jwt.sign(
-          { user_id: usuario._id, name },
-          process.env.TOKEN_KEY,
-          {
-            expiresIn: "1h",
-          }
-        );
-  
-        // save user token
-        usuario.save( usuario.token = token);
-  
-        // user
-        res.status(201).send({message: `${usuario.name} autenticado com sucesso. token de acesso: ${usuario.token}`});
-      } else {
-        res.status(404).send({message: `Usuario ou senha invalidos.`})};
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
 }
 
--- BACK-END/src/routes/userRoutes.js
+++ BACK-END/src/routes/userRoutes.js
@@ -7,8 +7,5 @@ const router = express.Router();
 router
  .get("/users", UserController.ListarUsers)
  .post("/users", UserController.CadastrarUser)
- .post("/register", UserController.RegisterUser)
- .post("/login", UserController.userLogin)
- 
 
 export default router;
