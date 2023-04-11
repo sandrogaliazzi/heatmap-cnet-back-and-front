2daf69a994f49aa8516cc58111c12ebae0a8908b
Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
2023-04-11 08:34:50 -0300

    pppoe

--- BACK-END/src/controllers/pppoeController.js
+++ BACK-END/src/controllers/pppoeController.js
@@ -26,6 +26,7 @@ class PppoeDataController {
     }
     static pppoeOnline = (req, res) => {
      let pppoe = req.body.pppoe;
+     console.log(req.body)
      const sshClient = new Client();
      console.log(process.env.HUAWEY_USERNAME);
      sshClient.connect({
@@ -33,7 +34,7 @@ class PppoeDataController {
       username: process.env.HUAWEY_USERNAME,
       password: `#${process.env.HUAWEY_PASSWORD}`,
      });
-
+     
      sshClient.on('ready', () => {
      sshClient.exec(`display access-user username ${pppoe}`, (err, stream) => {
       if (err) throw err;
@@ -43,8 +44,12 @@ class PppoeDataController {
       });
       stream.on('close', () => {
         if (output.includes('PPPoE')) {
+          console.log(output)
+          console.log(`este é o ${pppoe}`)
           res.status(201).send({message:'PPPoE is online.'});
         } else {
+          console.log(output)
+          console.log(`este é o ${pppoe}`)
           res.status(500).send({message: 'PPPoE is offline or not exist.'});
         }
         sshClient.end();
@@ -63,15 +68,90 @@ class PppoeDataController {
      });    
     }
 
-    static findAllPppoe = (req, res) =>{
-        try {
-              const docs = PppoeData.find({}, { _id: 0, pppoe: 1 });
-              const pppoeFields = docs.map(doc => doc.pppoe);
-              console.log(pppoeFields);
-            } catch (error) {
-              console.error(error);
-            }
-          }
+    static findAllPppoe = async (req, res) => {
+      try {
+        const docs = await PppoeData.find({}, { _id: 0, pppoe: 1 }).exec();
+        const pppoeFields = docs.map(doc => doc.pppoe);
+        console.log(pppoeFields);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    
+    
+    static findAllPppoeOnline = (req, res) => {
+      // Step 1: Get all PPPoE from the DB
+      PppoeData.find({}, { pppoe: 1 })
+        .lean()
+        .then((pppoeDocs) => {
+          const pppoeFields = pppoeDocs.map((doc) => doc.pppoe);
+          const onlinePppoes = [];
+          const offlinePppoes = [];
+    
+          // Step 2: Connect to SSH server and check PPPoE status for each PPPoE
+          const sshClient = new Client();
+          sshClient.on('error', (err) => {
+            console.error(`SSH error: ${err.message}`);
+            res.status(500).send({ message: 'Error checking PPPoE data' });
+          });
+          sshClient.on('close', () => {
+            console.log('SSH connection closed.');
+            res.status(200).json({ onlinePppoes, offlinePppoes });
+          });
+          console.log(`Connecting to SSH server at ${process.env.HUAWEI_HOST}...`);
+          sshClient.connect({
+            host: process.env.HUAWEY_HOST,
+            username: process.env.HUAWEY_USERNAME,
+            password: `#${process.env.HUAWEY_PASSWORD}`,
+           });
+    
+          sshClient.on('ready', () => {
+            let count = 0;
+            pppoeFields.forEach((pppoe) => {
+              sshClient.exec(`display access-user username ${pppoe}`, (err, stream) => {
+                if (err) {
+                  console.error(`Error checking PPPoE ${pppoe}: ${err.message}`);
+                  offlinePppoes.push(pppoe);
+                } else {
+                  let output = '';
+                  stream.on('data', (data) => {
+                    output += data.toString();
+                  });
+                  stream.on('close', () => {
+                    const regex = new RegExp(`PPPoE +${pppoe} +[A-Z]+`, 'i');
+                    const isOnline = regex.test(output);
+                    console.log(`PPPoE ${pppoe} is ${isOnline ? 'online' : 'offline'}`);
+                    if (isOnline) {
+                      onlinePppoes.push(pppoe);
+                    } else {
+                      offlinePppoes.push(pppoe);
+                    }
+                    count++;
+                    if (count === pppoeFields.length) {
+                      sshClient.end();
+                    }
+                  });
+                  stream.on('error', (err) => {
+                    console.error(`Error checking PPPoE ${pppoe}: ${err.message}`);
+                    offlinePppoes.push(pppoe);
+                    count++;
+                    if (count === pppoeFields.length) {
+                      sshClient.end();
+                    }
+                  });
+                }
+              });
+            });
+          });
+        })
+        .catch((error) => {
+          console.error(`Error getting PPPoE data: ${error.message}`);
+          res.status(500).send({ message: 'Error getting PPPoE data' });
+        });
+    };
+    
+    
+    
     }
 
     
