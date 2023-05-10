df6f11ace08d320943b36a91038cc268d520ccfe
Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
2023-05-10 11:39:39 -0300

    auth
    requisita token nas rotas

--- BACK-END/src/routes/pppoeRoutes.js
+++ BACK-END/src/routes/pppoeRoutes.js
@@ -1,16 +1,17 @@
 
 import express  from "express";
 import PppoeDataController from "../controllers/pppoeController.js";
+import auth from "../middleware/auth.js";
 
 
 const router = express.Router();
 
 router
- .post("/pppoecadastro", PppoeDataController.CadastroPppoe) // salva no banco os dados do pppoe.
- .get("/pppoeget", PppoeDataController.ListarPppoe)// requisita do banco os dados.
- .get("/pppoeonline", PppoeDataController.pppoeOnline) // verifica se esta online o pppoe.
- .post("/saveallpppoeonline", PppoeDataController.SavePppoeOnline) // save all pppoe
- .get("/getallpppoeonline", PppoeDataController.findAllPppoeOnline) // find all pppoe
+ .post("/pppoecadastro", auth, PppoeDataController.CadastroPppoe) // salva no banco os dados do pppoe.
+ .get("/pppoeget", auth, PppoeDataController.ListarPppoe)// requisita do banco os dados.
+ .get("/pppoeonline", auth, PppoeDataController.pppoeOnline) // verifica se esta online o pppoe.
+ .post("/saveallpppoeonline", auth, PppoeDataController.SavePppoeOnline) // save all pppoe
+ .get("/getallpppoeonline", auth, PppoeDataController.findAllPppoeOnline) // find all pppoe
  
  
  export default router;
