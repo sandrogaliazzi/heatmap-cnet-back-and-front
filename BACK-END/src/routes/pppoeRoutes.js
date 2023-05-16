c3cc4bd07aae180cc9420e2f0015935619e5b20a
Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
2023-05-16 16:29:27 -0300

    Update pppoeRoutes.js

--- BACK-END/src/routes/pppoeRoutes.js
+++ BACK-END/src/routes/pppoeRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router
  .post("/pppoecadastro", auth, PppoeDataController.CadastroPppoe) // salva no banco os dados do pppoe.
- .get("/pppoeget", PppoeDataController.ListarPppoe)// requisita do banco os dados.
+ .get("/pppoeget", auth, PppoeDataController.ListarPppoe)// requisita do banco os dados.
  .post("/pppoeonline", auth, PppoeDataController.pppoeOnline) // verifica se esta online o pppoe.
  .get("/getallpppoeonline", auth, PppoeDataController.findAllPppoeOnline) // verifica no concentrador quais estão online e retorna para o front.
  
