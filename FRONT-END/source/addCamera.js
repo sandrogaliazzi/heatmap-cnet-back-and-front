ad32c6e6a8e4a0953806ffee3d6ea3c7419769fe
sandrogaliazzi <sroque.roque@gmail.com>
2023-06-26 17:18:30 -0300

    adicionado validação de imagens do formulário das cameras

--- FRONT-END/source/addCamera.js
+++ FRONT-END/source/addCamera.js
@@ -15,18 +15,51 @@ async function sendCamera(data) {
   return response;
 }
 
+$("#formFile").addEventListener("change", function () {
+  const [file1, file2] = this.files;
+
+  if (file1 && file2) {
+    const imgUrl1 = URL.createObjectURL(file1);
+    const imgUrl2 = URL.createObjectURL(file2);
+
+    renderImgThumb(createFilePreviewItems([imgUrl1, imgUrl2]));
+
+    $("#formFileCheck").classList.remove("d-none");
+    $("#fileIputValidationMsg").classList.add("d-none");
+  } else {
+    alert("Apenas um arquivo selecionado");
+  }
+});
+
+function createFilePreviewItems(files) {
+  return files.reduce((htmlText, item) => {
+    htmlText += `<img src="${item}" width="200px" height="200px">`;
+
+    return htmlText;
+  }, "");
+}
+
+function renderImgThumb(htmlTemplateStr) {
+  $("#imgPreviewContainer").innerHTML = htmlTemplateStr;
+}
+
 $("#addCameraForm").addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const data = new FormData(this);
+  if ($("#checkFiles").checked) {
+    const data = new FormData(this);
 
-  const response = await sendCamera(data);
+    const response = await sendCamera(data);
 
-  if (response.status === 200) {
-    triggerToast("Dados enviados com sucesso", true);
-    this.reset();
-    modal.hide();
+    if (response.status === 200) {
+      triggerToast("Dados enviados com sucesso", true);
+      $("#fileIputValidationMsg").classList.remove("d-none");
+      this.reset();
+      modal.hide();
+    } else {
+      triggerToast("Ocorreu um erro ao enviar os dadoas", false);
+    }
   } else {
-    triggerToast("Ocorreu um erro ao enviar os dadoas", false);
+    triggerToast("Confirme os dados antes de enviar!", false);
   }
 });
